6ac934e84bc5a25bf87defaf2a93eda0c2292110
JVRicci <jvricci99@gmail.com>
2026-04-26 22:52:31 -0300

    rename schema folder

--- src/schema/user_schema.py
+++ src/schema/user_schema.py
@@ -1,13 +1,13 @@
-from pydantic import BaseModel, Field
-from src.utils.email_regex import email_regex
+from pydantic import BaseModel, EmailStr, Field
 
 
 class UserRegisterValidator(BaseModel):
-    username: str = Field(..., min_length=3)
-    age: int = Field(..., gt=0)
-    email: str = Field(..., pattern=email_regex)
+    username: str = Field(..., min_length=3, description="User's username")
+    age: int = Field(..., gt=0, description="User's age, must be greater than 0")
+    email: EmailStr = Field(..., description="User's email address")
     uf: str = Field(
         ...,
         min_length=2,
         max_length=2,
+        description="User's state abbreviation (e.g., 'SP' for São Paulo)",
     )
